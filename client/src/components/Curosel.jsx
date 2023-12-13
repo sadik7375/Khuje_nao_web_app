cabf229c2e9b5fbf85c988c04583c238db8b1b42
sadik7375 <68754591+sadik7375@users.noreply.github.com>
2023-12-13 22:43:00 +0600

    all

--- client/src/components/Curosel.jsx
+++ client/src/components/Curosel.jsx
@@ -104,13 +104,13 @@ function Carousel() {
           fill="none"
           viewBox="0 0 6 10"
         >
-          <path
+         {/* <path
             stroke="currentColor"
             strokeLinecap="round"
             strokeLinejoin="round"
             strokeWidth={2}
             d="m1 9 4-4-4-4"
-          />
+          />*/}
         </svg>
         <span className="sr-only">Next</span>
       </span>
